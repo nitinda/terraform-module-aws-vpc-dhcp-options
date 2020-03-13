73c95ee5b3af032892535def4c6e07bed20c9cda
nitin-das <nitin.das@thomson.co.uk>
2020-03-13 17:30:50 +0000

    Modified Code

--- vpc-dhcp-options.tf
+++ vpc-dhcp-options.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc_dhcp_options" "vpc_dhcp_options" {
-  domain_name         = var.domain_name
+  domain_name         = var.domain_name == null ? "${data.aws_region.region.name}.compute.internal" : var.domain_name
   domain_name_servers = var.domain_name_servers
   ntp_servers         = var.ntp_servers
   tags                = var.tags
